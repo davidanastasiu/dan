bfb7b2ad74d6ba278032b2f6d954b85eddc6853a
Vladimir Ceban <v3ceban@proton.me>
2024-12-05 20:18:37 -0800

    remove print statement

--- run.py
+++ run.py
@@ -179,7 +179,6 @@ class Options:
         expr_dir = os.path.join(self.opt.outf, self.opt.name, "train")
         val_dir = os.path.join(self.opt.outf, self.opt.name, "val")
         test_dir = os.path.join(self.opt.outf, self.opt.name, "test")
-        print("opt.name: ", self.opt.name)
 
         if not os.path.isdir(expr_dir):
             os.makedirs(expr_dir)
